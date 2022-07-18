51801d920df9d3127182ba762e6538a77b9001bf
Ariel <ariel02041993@gmail.com>
2022-07-17 22:01:56 -0300

    segundo commit de backend - create users

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,6 +2,10 @@ const UsersController = require('../controllers/usersController');
 const { app } = require('../server');
 
 module.exports = (app) => {
-
+    
+    // TRAER DATOS
     app.get('/api/users/getAll', UsersController.getAll);
+    
+    // GUARDAR DATOS
+    app.post('/api/users/create', UsersController.register);
 }
